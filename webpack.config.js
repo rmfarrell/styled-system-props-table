769afd0e27eac52a40beecb1c725aeab29306990
Ryan <rmfarrell@gmail.com>
2020-10-05 11:59:01 -0400

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,36 @@
 module.exports = {
+    devtool: '#eval-source-map',
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
             }
-        ]
-    }
+        ],
+    },
+    output: {
+        library: 'StyledSystemPropsTable',
+        libraryTarget: 'umd',
+        filename: 'main.js'
+    },
+    externals: [
+        // nodeExternals(),
+        {
+            react: {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            },
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom'
+            }
+        }
+    ]
 }
